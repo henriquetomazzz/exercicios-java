f29f24053267f024a0685dcf1a8c78cbad5026cd
henrique.tomaz <carlos.henrique.ext@synchro.com.br>
2024-11-04 16:04:39 -0300

    chore: corrige test

--- src/main/java/poo/atividade04/ContaBancaria.java
+++ src/main/java/poo/atividade04/ContaBancaria.java
@@ -4,38 +4,39 @@ public class ContaBancaria {
     private double saldo;
     private double chequeEspecial;
 
-    public ContaBancaria(double chequeEspecial){
+    public ContaBancaria(double chequeEspecial) {
         this.saldo = 0.0;
         this.chequeEspecial = chequeEspecial;
     }
 
-    //depositar
-    public void depositar(double valor){
-        if (valor > 0){
-            valor += saldo;
-        }else{
-            throw new IllegalArgumentException("Valor de saque deve ser positivo.");
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            throw new IllegalArgumentException("Valor de depósito deve ser positivo.");
         }
     }
-    //sacar
-    public void sacar(double valor){
-        if (valor <= 0){
-            throw new IllegalThreadStateException("Valor de saque deve ser positivo.");
+
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor de saque deve ser positivo.");
         }
-        if (valor <= saldo){
+        if (valor <= saldo) {
             saldo -= valor;
-        }else if(valor <= saldo + chequeEspecial){
+        } else if (valor <= saldo + chequeEspecial) {
             double saldoResultante = valor - saldo;
             saldo = 0;
             chequeEspecial -= saldoResultante;
-        }else{
-            throw new IllegalThreadStateException("Saldo insuficiente, incluindo o cheque especial");
+        } else {
+            throw new IllegalArgumentException("Saldo insuficiente, incluindo o cheque especial.");
         }
     }
-    public double getSaldo(){
+
+    public double getSaldo() {
         return saldo;
     }
-    public double getChequeEspecialDisponivel(){
+
+    public double getChequeEspecialDisponivel() {
         return chequeEspecial;
     }
 }
