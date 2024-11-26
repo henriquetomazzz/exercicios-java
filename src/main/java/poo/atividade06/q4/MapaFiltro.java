b6470b25be6974371e89a3448dc8096b1f97c82c
henriquetomazzz <henriquetomaz021@gmail.com>
2024-11-26 16:10:14 -0300

    feat: resolve questão referente aos metodos map e filter em stream

--- src/main/java/poo/atividade06/q4/MapaFiltro.java
+++ src/main/java/poo/atividade06/q4/MapaFiltro.java
@@ -1,11 +1,11 @@
-package main.java.poo.atividade06;
+package main.java.poo.atividade06.q4;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MapaFiltro {
-    public static void mapaFiltro(String[] args) {
+    public static void main(String[] args) {
         List<String> palavras = Arrays.asList("Maçã", "Banana", "Acerola", "Formiga", "Gato", "Arara");
 
         List<String> resultado = palavras.stream()
@@ -16,4 +16,3 @@ public class MapaFiltro {
         System.out.println(resultado);
     }
 }
-
