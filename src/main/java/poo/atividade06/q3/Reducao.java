e7adceff1618960b017f7a866325f9c108b182d7
henriquetomazzz <henriquetomaz021@gmail.com>
2024-11-26 16:17:10 -0300

    chore: resolve questão referente ao reduce em stream

--- src/main/java/poo/atividade06/q3/Reducao.java
+++ src/main/java/poo/atividade06/q3/Reducao.java
@@ -1,7 +1,15 @@
 package main.java.poo.atividade06.q3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Reducao {
     public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
+        int resultado = numeros.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println(resultado);
     }
 }
