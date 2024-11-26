3e936ca6136c6bcf05a06ab2186c42aceb418dd6
henriquetomazzz <henriquetomaz021@gmail.com>
2024-11-26 13:55:02 -0300

    feat: resolve questão referente a stream

---
+++ src/main/java/poo/atividade06/MapaFiltro.java
@@ -0,0 +1,19 @@
+package main.java.poo.atividade06;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapaFiltro {
+    public static void mapaFiltro(String[] args) {
+        List<String> palavras = Arrays.asList("Maçã", "Banana", "Acerola", "Formiga", "Gato", "Arara");
+
+        List<String> resultado = palavras.stream()
+                .map(String::toUpperCase)
+                .filter(p -> p.startsWith("A"))
+                .collect(Collectors.toList());
+
+        System.out.println(resultado);
+    }
+}
+
